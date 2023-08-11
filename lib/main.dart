fb371bc7c40fcda5b48911af99a9f7e88d999aa0
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-11 22:01:12 +0500

    2. create column

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
       home: Scaffold(
         body: Container(
           margin: const EdgeInsets.all(14),
-          height: 999,
+          width: 999,
           decoration: BoxDecoration(
             color: const Color.fromARGB(255, 85, 142, 207),
             border: Border.all(
@@ -16,21 +16,19 @@ void main() {
               width: 10,
             ),
           ),
-          child: Row(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               for (int i = 0; i < 3; i++)
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 7,
-                      ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 999,
+                  height: 50, 
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 7,
                     ),
                   ),
                 ),
