37c4446a4fbd02d8d228602b78519a8b4801574b
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-12 21:10:43 +0500

    userNmae ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,70 +5,41 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("AppName"),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              for (int i = 0; i < 5; i++)
-                Container(
-                  margin: const EdgeInsets.only(left: 20, top: 12, right: 20),
-                  height: 110,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black26,
-                      width: 1,
+        backgroundColor: const Color.fromARGB(255, 197, 197, 197),
+        body: Column(
+          children: [
+            for (int i = 0; i < 5; i++)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 30, top: 20),
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: const Color.fromARGB(255, 120, 148, 220),
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      size: 33,
+                      color: Color.fromARGB(255, 45, 16, 159),
                     ),
                   ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7xADuNmSkk1VTU_enYZEQMVYkIvLpawltfA&usqp=CAU",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.all(10),
-                              child: const Text(
-                                "Restaurant Name",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16, // Matn o'lchamini belgilash
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(10),
-                              child: const Text(
-                                "Address",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16, // Matn o'lchamini belgilash
-                                ),
-                              ),
-                            ),
-                            // Boshqa matnlar
-                          ],
-                        ),
-                      ),
-                    ],
+                  const Text(
+                    "   Usernmae",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
                   ),
-                ),
-              // Boshqa elementlar
-            ],
-          ),
+                  const Text(
+                    "    email",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+          ],
         ),
       ),
     ),
