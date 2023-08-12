9df9b5f74f24802671a6a634d08268facb4d97ae
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-12 16:40:09 +0500

    Gird - 20

--- lib/main.dart
+++ lib/main.dart
@@ -6,102 +6,181 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 350,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade300,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade300,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      width: 200,
+                      height: 200,
+                      margin: const EdgeInsets.all(10),
                     ),
-                  ),
+                    Row(
+                      children: [
+                        for (int i = 0; i < 2; i++)
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue.shade300,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            width: 90,
+                            height: 80,
+                            margin: const EdgeInsets.all(10),
+                          )
+                      ],
+                    ),
+                  ],
                 ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 80,
-                  height: 300,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade300,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade300,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.all(10),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 80,
-                        height: 180,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade300,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade300,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
+                          width: 100,
+                          height: 180,
+                          margin: const EdgeInsets.all(10),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        for (int i = 0; i < 3; i++)
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue.shade300,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            width: 100,
+                            height: 87,
+                            margin: const EdgeInsets.all(10),
+                          ),
+                      ],
+                    ),
+                  ],
+                )
               ],
             ),
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 80,
-                  height: 300,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade300,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade300,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
+                          width: 200,
+                          height: 90,
+                          margin: const EdgeInsets.all(10),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 80,
-                        height: 180,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade300,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade300,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 350,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade300,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
+                          width: 100,
+                          height: 90,
+                          margin: const EdgeInsets.all(10),
+                        )
+                      ],
                     ),
-                  ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            for (int i = 0; i < 2; i++)
+                              Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue.shade300,
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                width: 90,
+                                height: 90,
+                              )
+                          ],
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue.shade300,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          width: 210,
+                          height: 200,
+                          margin: const EdgeInsets.all(10),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade300,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      width: 100,
+                      height: 200,
+                      margin: const EdgeInsets.all(10),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade300,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      width: 100,
+                      height: 90,
+                      margin: const EdgeInsets.all(10),
+                    ),
+                  ],
                 ),
               ],
-            ),
+            )
           ],
         ),
       ),
