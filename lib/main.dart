107279e9b6b0400bec96ce4258b7267e815ec57c
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-08-12 23:04:01 +0500

    tik tok ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,11 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    margin: const EdgeInsets.only(bottom: 100),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          "https://static.vecteezy.com/system/resources/previews/023/986/555/original/instagram-logo-instagram-logo-transparent-instagram-icon-transparent-free-free-png.png",
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(
-                      top: 550,
-                    ),
-                    width: 100,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          "https://assets.stickpng.com/thumbs/61fae2ba95e6ca00047b4f11.png",
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(
-                      top: 500,
-                    ),
-                    child: const Text(
-                      "form",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+          child: Image.network(
+            "https://w7.pngwing.com/pngs/261/691/png-transparent-tiktok-icon-black-1-hd-logo-thumbnail.png",
+            width: 150,
           ),
         ),
       ),
